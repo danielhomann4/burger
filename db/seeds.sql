40c2a36b096b7a454c952420b813a6c53259f179
Daniel Homann <danielhomann4@gmail.com>
2019-02-25 16:10:00 -0800

    set it up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Western Bacon Cheeseburger", FALSE);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Double Cheeseburger", FALSE);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Mac & Cheese Cheeseburger", FALSE);
